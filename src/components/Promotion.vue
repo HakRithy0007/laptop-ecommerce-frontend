e8a7a9c894a4b320c3fc56d44bf8b2674aa8fcc0
hakrithy <hakrithykim2020@gmail.com>
2025-09-10 10:20:22 +0700

    add code

--- src/components/Promotion.vue
+++ src/components/Promotion.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="w-full h-full flex justify-center items-center py-2 rounded-md px-6 mt-4 pb-6 bg-[#faf9f9] shadow-xl">
-
     <div class="w-full h-full grid grid-cols-4 gap-4 mt-2">
       <div v-for="(feature, index) in features" :key="index"
         class="w-full h-full flex justify-center items-center rounded-lg shadow-xl cursor-pointer transform transition duration-300 hover:scale-101">
