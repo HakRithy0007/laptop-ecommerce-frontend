54b072791fc036b831047589b7b4880669a78227
hakrithy <hakrithykim2020@gmail.com>
2025-09-16 10:57:07 +0700

    add code

--- src/components/Promotion.vue
+++ src/components/Promotion.vue
@@ -4,7 +4,6 @@
       <div v-for="(feature, index) in features" :key="index"
         class="w-full h-full flex justify-center items-center rounded-lg shadow-xl cursor-pointer transform transition duration-300 hover:scale-101">
         <span>
-          
           <img :src="feature.img" class="w-full h-full rounded-lg" />
         </span>
       </div>
